d57a1b5cdd077ef94c8bd368ee98c2753c2a54eb
ajing <ajingnk@gmail.com>
2015-02-03 20:37:06 -0500

    initial files

---
+++ lab4.R
@@ -0,0 +1,3 @@
+a<-read.table("/home/remills/Projects/sandbox/coursera/rs8067378_ENSG00000172057.4.txt", header=F)
+names(a) = c("sample", "geno", "exp")
+boxplot(exp~geno, data=a, xlab="rs8067378 genotype", ylab="ENSG00000172057.4 (RPKM)", notch=T)
